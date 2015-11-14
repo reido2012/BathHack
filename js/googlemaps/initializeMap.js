3124a060b807cec34a93f6225620188ad46fc2be
James Thompson <thompsonjamesrichard@gmail.com>
2015-11-14 21:47:37 +0000

    better init of map

--- js/googlemaps/initializeMap.js
+++ js/googlemaps/initializeMap.js
@@ -1,7 +1,6 @@
 function initializeMap() {
     
-    var mapBox = document.getElementById('map-here');
-    
+
     var mapOptions = {
         center: new google.maps.LatLng(clientLocation.Latitude, clientLocation.Longitude),
         zoom: 7
