93292e5928a19b0a503e210250c9b757f755c484
James Thompson <thompsonjamesrichard@gmail.com>
2015-11-15 02:41:18 +0000

    stuff

---
+++ uploadedVideos.php
@@ -0,0 +1,20 @@
+
+<?php
+	$tablename = "videos";
+
+	try {
+    $stmt = $db->prepare("SELECT * FROM $tablename");
+    $stmt->execute();
+    
+
+    // set the resulting array to associative
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    	
+		$id=$result['videoId'];
+		$name = $result['videoName'];
+		echo "<a href='watch.php?id=$id'>$name</a><br/>";
+    }
+    
+	}catch(PDOException $e) {
+	    echo "Error: " . $e->getMessage();
+	}
