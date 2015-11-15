3ab6b417b0666971af65e91313bd5a41119af642
James Thompson <thompsonjamesrichard@gmail.com>
2015-11-15 02:30:25 +0000

    added video form

---
+++ uploadVideo.php
@@ -0,0 +1,72 @@
+<?php
+
+function uploadVideo() {
+    /*
+    $servername = "bathhack.cloudcell.co.uk";
+    $username = "bathhackapp";
+    $password = "fe2184fe2184";
+    $dbName = "panic-button";
+    */
+
+    $tablename = "videos";
+    
+
+    try {
+
+            $plsDrop = "DROP TABLE IF EXISTS $tablename";
+            $db->exec($plsDrop);
+
+             // sql to create table
+            $sql = "CREATE TABLE $tablename(
+                VIDEO LONGBLOB,
+                videoId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                videoName VARCHAR(255),	
+                videoURL VARCHAR(255) 
+            )";
+
+            $db->exec($sql);
+
+
+
+                if(isset($_POST['submit'])) {
+
+                $name = $_FILES['fileToUpload']['name'];
+                $temp = $_FILES['fileToUpload']['tmp_name'];
+
+        $message = '';
+            switch( $_FILES['fileToUpload']['error'] ) {
+                case UPLOAD_ERR_OK:
+                    $message = false;;
+                    break;
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    $message .= ' - file too large (limit of xxx bytes).';
+                    break;
+                case UPLOAD_ERR_PARTIAL:
+                    $message .= ' - file upload was not completed.';
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    $message .= ' - zero-length file uploaded.';
+                    break;
+                default:
+                    $message .= ' - internal error #'.$_FILES['newfile']['error'];
+                    break;
+            }
+
+
+                move_uploaded_file($temp, "./uploaded/".$name);
+                $url = "http://localhost/Video%20Uploader/uploaded/$name";
+
+                 $stmt = ("INSERT INTO $tablename (videoId, videoURL, videoName) VALUES ('','$url', '$name')");
+
+                $db -> exec($stmt);
+                echo "<br/>".$name." has been uploaded<br/>";
+            } else {
+                echo "<br />Please upload a file<br/>";
+            }
+        }
+        catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
+}
